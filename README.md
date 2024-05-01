b03740d7136cd6e3cebba8675b09677d76ce7dd6
Dizaraj Dey <dizaraj@gmail.com>
2024-05-01 23:48:43 +0600

    Blood Request Form added

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Installation guideline will be provided after completion of the project. So, sta
 ```
 https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css
 https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css
+https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css
 ```
 ## JS Files
 ## Used
@@ -27,9 +28,6 @@ https://dummyjson.com/users
 ```
 ## Will be Used
 ```
-https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css
-https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css
-https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css
 https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css
 https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css
 ```
--- userdata.js
+++ userdata.js
@@ -11,18 +11,39 @@ fetch("https://dummyjson.com/users")
   .then((data) => {
     // console.log(data.users);
     var table = document.getElementById("usersTable");
-    data["users"].forEach((user) => {
+    data["users"].slice(0, 15).forEach((user) => {
       var row = table.insertRow(-1);
       var cell1 = row.insertCell(0);
       var cell2 = row.insertCell(1);
       var cell3 = row.insertCell(2);
       var cell4 = row.insertCell(3);
       var cell5 = row.insertCell(4);
-      cell1.innerHTML = user.firstName;
+      cell1.innerHTML = user.firstName + " " + user.lastName;
       cell2.innerHTML = user.age;
       cell3.innerHTML = user.bloodGroup;
       cell4.innerHTML = getRandomDate().toLocaleDateString();
       cell5.innerHTML = user.phone;
+    });
+  })
+  .catch((error) => console.error("Error:", error));
+
+fetch("https://dummyjson.com/users")
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data.users);
+    var table = document.getElementById("requestTable");
+    data["users"].slice(0, 5).forEach((user) => {
+      var row = table.insertRow(-1);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      cell1.innerHTML = "<i class='fa-solid fa-droplet'></i>";
+      cell2.innerHTML = user.firstName + " " + user.lastName;
+      cell3.innerHTML = user.bloodGroup;
+      cell4.innerHTML = user.address.address;
+      cell5.innerHTML = getRandomDate().toLocaleDateString();
     });
   })
   .catch((error) => console.error("Error:", error));
